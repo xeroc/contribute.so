62dfeadf8f23a7798126467bcbd049731d206a1c
Fabian Schuh <mail@xeroc.org>
2025-10-31 19:34:46 +0100

    🧹 chore: add eruda

--- packages/app/src/app/layout.tsx
+++ packages/app/src/app/layout.tsx
@@ -22,6 +22,13 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
       <body>
         <Providers>{children}</Providers>
         <Script defer data-domain="contribute.so" src="https://p.chainsquad.com/js/script.js" />
+        <Script src="https://cdn.jsdelivr.net/npm/eruda" />
+        <Script
+          id="eruda"
+          dangerouslySetInnerHTML={{
+            __html: `eruda.init();`,
+          }}
+        />
       </body>
     </html>
   )
